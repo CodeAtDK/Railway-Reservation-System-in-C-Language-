5b5b263ec883d317fbdb0713d7e0c60f569ccdaa
CodeAtDK <112199494+CodeAtDK@users.noreply.github.com>
2022-12-02 15:17:54 +0530

    Add files via upload

---
+++ mainfile.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int Operation;
+
+    printf("1.Ticket Booking\n2.Cancel Ticket");
+    scanf("%d",&Operation);
+
+    switch(Operation){
+
+        case 1:
+
+            break;
+
+        case 2:
+
+            break;
+
+        default:
+
+            printf("You have Seleleted wrong Option");    
+    }
+
+    return 0;
+}
